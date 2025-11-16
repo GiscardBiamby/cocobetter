3a02849e66cfb0eb5e29037f77dd7500ceebdda5
Giscard Biamby <giscard@gmail.com>
2025-11-16 09:57:02 +0000

    Merge in changes from the newer pycocotools github repo (old one is dead, new is this: https://github.com/ppwwyyxx/cocoapi)

--- PythonAPI/setup.py
+++ PythonAPI/setup.py
@@ -1,24 +1,29 @@
-import numpy as np
 from pybind11 import get_cmake_dir
+
 # Available at setup time due to pyproject.toml
-from pybind11.setup_helpers import Pybind11Extension, build_ext
 from setuptools import Extension, setup
 
-# To compile and install locally run "python setup.py build_ext --inplace"
-# To install library to Python site-packages run "python setup.py build_ext install"
+# Optional: only needed if you actually enable the pybind11 extension below
+from pybind11.setup_helpers import Pybind11Extension, build_ext  # noqa: F401
+
+
+def get_numpy_includes():
+    import numpy as np
+
+    return [np.get_include(), "../common"]
+
 
 ext_modules = [
     Extension(
         "pycocotools._mask",
         sources=["../common/maskApi.c", "pycocotools/_mask.pyx"],
-        include_dirs=[np.get_include(), "../common"],
+        include_dirs=get_numpy_includes(),
         extra_compile_args=["-Wno-cpp", "-Wno-unused-function", "-std=c99"],
     ),
     # Pybind11Extension(
     #     "pycocotools._eval",
     #     sources=["../fastcocoeval/cocoeval.cpp"],
-    #     include_dirs=[np.get_include(), "../fastcocoeval"],
-    #     # extra_compile_args=["-Wno-cpp", "-Wno-unused-function", "-std=c99"],
+    #     include_dirs=get_numpy_includes(),
     # )
 ]
 
@@ -26,7 +31,11 @@ setup(
     name="pycocotools",
     packages=["pycocotools"],
     package_dir={"pycocotools": "pycocotools"},
-    install_requires=["setuptools>=18.0", "cython>=0.27.3", "matplotlib>=2.1.0"],
+    install_requires=[
+        "setuptools>=18.0",
+        "cython>=0.27.3",
+        "matplotlib>=2.1.0",
+    ],
     version="2.0.2",
     ext_modules=ext_modules,
 )
